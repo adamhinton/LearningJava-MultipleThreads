2e74d4d5edf8a96c07764d80c7fc4b85ea866cfc
adamd <adam.d.hinton@gmail.com>
2024-04-27 11:16:22 -0400

    Stopw

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -1,9 +1,42 @@
 package dev.lpa;
 
+import java.util.concurrent.TimeUnit;
+
 public class Main {
     public static void main(String[] args) {
 
 
 
+    }
+}
+
+class StopWatch{
+    private TimeUnit timeUnit;
+
+    public StopWatch(TimeUnit timeUnit) {
+        this.timeUnit = timeUnit;
+    }
+
+    public void countDown(int unitCount){
+        String threadName = Thread.currentThread().getName();
+
+        ThreadColor threadColor = ThreadColor.ANSI_RESET;
+        try{
+            threadColor = ThreadColor.valueOf(threadName);
+        }
+        catch(IllegalArgumentException ignore){
+            // Ignore if user passes bad color name
+        }
+
+        String color = threadColor.color();
+        for (int i=unitCount; i>0; i--){
+            try{
+                timeUnit.sleep(1);
+            }
+            catch(InterruptedException e){
+                e.printStackTrace();
+            }
+            System.out.printf("%s%s Thread : i = %d%n", color, threadName, i);
+        }
     }
 }
