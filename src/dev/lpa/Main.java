a50f7398ed0a6f68fc9dc4eb7c560b958701d200
adamd <adam.d.hinton@gmail.com>
2024-04-27 19:19:46 -0400

    init commit Sat night

--- src/dev/lpa/Main.java
+++ src/dev/lpa/Main.java
@@ -5,35 +5,49 @@ import java.util.concurrent.TimeUnit;
 public class Main {
     public static void main(String[] args) {
 
+        StopWatch stopWatch = new StopWatch(TimeUnit.SECONDS);
 
+        Thread green = new Thread(stopWatch::countDown, ThreadColor.ANSI_GREEN.name());
+        Thread purple = new Thread(() -> stopWatch.countDown(7), ThreadColor.ANSI_PURPLE.name());
+        Thread red = new Thread(stopWatch::countDown, ThreadColor.ANSI_RED.name());
+
+
+
+        green.start();
+        purple.start();
+        red.start();
 
     }
 }
 
-class StopWatch{
+class StopWatch {
     private TimeUnit timeUnit;
+    private int i;
 
     public StopWatch(TimeUnit timeUnit) {
         this.timeUnit = timeUnit;
     }
 
-    public void countDown(int unitCount){
+    public void countDown() {
+        // Default is 5
+        countDown(5);
+    }
+
+    public void countDown(int unitCount) {
         String threadName = Thread.currentThread().getName();
 
         ThreadColor threadColor = ThreadColor.ANSI_RESET;
-        try{
+        try {
             threadColor = ThreadColor.valueOf(threadName);
-        }
-        catch(IllegalArgumentException ignore){
+        } catch (IllegalArgumentException ignore) {
             // Ignore if user passes bad color name
         }
 
         String color = threadColor.color();
-        for (int i=unitCount; i>0; i--){
-            try{
+        for (i = unitCount; i > 0; i--) {
+            try {
                 timeUnit.sleep(1);
-            }
-            catch(InterruptedException e){
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
             System.out.printf("%s%s Thread : i = %d%n", color, threadName, i);
